2832375d766a46e9a3de18f7eb055ac4619099f3
Teddy Stanowski <tstanowski31@gmail.com>
2017-08-24 15:22:42 -0700

    pass unhappy sign up path

--- spec/features/viewer_signs_up_spec.rb
+++ spec/features/viewer_signs_up_spec.rb
@@ -7,6 +7,7 @@ feature 'sign up' do
   scenario 'specifying valid and required information' do
     visit root_path
     click_link 'Sign Up'
+
     fill_in 'First name', with: 'Teddy'
     fill_in 'Last name', with: 'Stanowski'
     fill_in 'Email', with: 'teddy@teddy.com'
@@ -15,18 +16,28 @@ feature 'sign up' do
     fill_in 'Password confirmation', with: 'password1'
     click_button 'Sign up'
 
-    # expect(page).to have_content("Welcome! You have signed up successfully.")
     expect(page). to have_content("Sign Out")
   end
-  
-  # scenario 'does not fill out all information' do
-  #   visit root_path
-  #   click_link 'Sign Up'
-  #   fill_in 'First name', with: 'Teddy'
-  #   fill_in 'Username', with: 'teddys31'
-  #   fill_in 'Password', with: 'password1'
-  #   fill_in 'Password confirmation', with: 'password1'
-  #
-  #    expect(flash[:error]).to
-  # end
+
+  scenario 'does not fill out all information' do
+    visit root_path
+    click_link 'Sign Up'
+    click_button 'Sign up'
+
+    expect(page).to have_content("can't be blank")
+    expect(page).to_not have_content("Sign Out")
+  end
+
+  scenario 'password confirmation does not match' do
+    visit root_path
+    click_link 'Sign Up'
+
+    fill_in 'Password', with: 'password1'
+    fill_in 'Password confirmation', with: 'password'
+    click_button 'Sign up'
+
+    expect(page). to have_content("doesn't match")
+    expect(page).to_not have_content("Sign Out")
+  end
+
 end
