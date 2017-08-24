4b114cd619fddc34b1d720cb3a0fc4e481b925e0
Teddy Stanowski <tstanowski31@gmail.com>
2017-08-24 11:24:31 -0700

    passed happy user sign up path

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+
+  # def create
+  #   @user = User.new(user_params)
+  #   if @user.save
+  #     redirect_to root_path
+  #   else
+  #     render :nothing => true
+  #   end
+  # end
+
+end
---
+++ spec/features/viewer_signs_up_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+feature 'sign up' do
+
+  scenario 'specifying valid and required information' do
+    visit root_path
+    click_link 'Sign Up'
+    fill_in 'First name', with: 'Teddy'
+    fill_in 'Last name', with: 'Stanowski'
+    fill_in 'Email', with: 'teddy@teddy.com'
+    fill_in 'Username', with: 'teddys31'
+    fill_in 'Password', with: 'password1'
+    fill_in 'Password confirmation', with: 'password1'
+    click_button 'Sign up'
+
+    # expect(page).to have_content("Welcome! You have signed up successfully.")
+    expect(page). to have_content("Sign Out")
+  end
+
+  # scenario 'does not fill out all information' do
+  #   visit root_path
+  #   click_link 'Sign Up'
+  #   fill_in 'First name', with: 'Teddy'
+  #   fill_in 'Username', with: 'teddys31'
+  #   fill_in 'Password', with: 'password1'
+  #   fill_in 'Password confirmation', with: 'password1'
+  #
+  #    expect(flash[:error]).to
+  # end
+end
