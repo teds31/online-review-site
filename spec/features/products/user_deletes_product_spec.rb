fddcc237b26ed05c18bd6e2e9c13f04b140fbcfb
Teddy Stanowski <tstanowski31@gmail.com>
2017-09-03 17:04:38 -0700

    pass happy and unhappy tests for deleting a product

--- spec/features/products/user_deletes_product_spec.rb
+++ spec/features/products/user_deletes_product_spec.rb
@@ -12,7 +12,7 @@ feature 'authenticated user deletes product they added' do
   # View link to delete the product
   # Product deletes and takes me back to index
 
-  scenario 'authenticated user views delete link on product page' do
+  scenario 'authenticated user who owns product resource views delete link on product page' do
     user = FactoryGirl.create(:user)
     login_as(user, :scope => :user)
     wine = FactoryGirl.create(:product, user_id: user.id)
@@ -22,7 +22,7 @@ feature 'authenticated user deletes product they added' do
     expect(page).to have_content("Delete Wine")
   end
 
-  scenario 'authenticated user deletes product from website' do
+  scenario 'authenticated user who owns product resource deletes product from website' do
     user = FactoryGirl.create(:user)
     login_as(user, :scope => :user)
     wine = FactoryGirl.create(:product, user_id: user.id)
@@ -33,4 +33,14 @@ feature 'authenticated user deletes product they added' do
     expect(page).to have_content("Wine has been removed.")
     expect(page).to have_content("Wine List")
   end
+
+  scenario 'authenticated user who does not own product resource cannot see delete link' do
+    user = FactoryGirl.create(:user)
+    wine = FactoryGirl.create(:product)
+    visit root_path
+    click_link wine.name
+
+    expect(page).to_not have_content("Delete Wine")
+  end
+
 end
