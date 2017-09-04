8933efcd50834d4f6f59714edeb28eecd3813c84
Teddy Stanowski <tstanowski31@gmail.com>
2017-09-03 17:13:29 -0700

    write tests to add a review to product

---
+++ spec/features/reviews/user_adds_review_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+feature 'authenticted user adds a review to a product page' do
+
+  # User Story
+  # As an authenticated user
+  # I want to write a review for a wine I have tried
+  #
+  # Acceptance Criteria
+  # View product page and see a link that says leave a review below the product
+  # Click link and see a form to fill out and submit button to submit review
+  # Review is posted under the product for everyone to see
+
+  xscenario 'authenticated user sees add a review link on product page' do
+
+  end
+
+  xscenario 'authenticated user fills out form and submits review to product page' do
+
+  end
+
+  xscenario 'authenticated user does not fill out form properly and recieves and error' do
+    
+  end
+end
