2558e7dae21932db2008c15afde3b1e9ec80206a
Teddy Stanowski <tstanowski31@gmail.com>
2017-09-05 14:10:22 -0700

    write happy and unhappy path tests

---
+++ spec/features/reviews/user_edits_review_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+feature 'authenticted user edits their own review' do
+
+  # User Story
+  # As an authenticated user,
+  # I want to update a review I made on a wine product
+  #
+  # Acceptance Criteria
+  # If user owns the review resource, they will see an edit review link
+  # user then can change the title and the body of the review
+  # once they save, they will see the changes reflected
+  # all forms must be filled out correctly
+
+  xscenario 'authenticated user sees edit link of their own reveiw' do
+
+  end
+
+  xscenario 'authenticated user edits review and submits' do
+
+  end
+
+  xscenario 'authenticated user gets error if they do not fill out the form correctly' do
+
+  end
+
+end
