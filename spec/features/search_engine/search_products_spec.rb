1b1b776f80889a1e8ae5184bc78c010fcaafa4ff
Teddy Stanowski <tstanowski31@gmail.com>
2017-09-06 13:07:51 -0700

    write tests

---
+++ spec/features/search_engine/search_products_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+feature 'user is able to search for wines on site through search engine' do
+  # User Story
+  # As an un-authenticated user,
+  # I want to search wines that are listed on the site
+  #
+  # Accepetance Criteria
+  # I see the search engine bar below the links
+  # I type in any type of wine and hit search button
+  # Takes me to a new page displaying all the results
+  # If I want to run another search, I can use the search bar at the top again
+  # I cannot search with a blank query
+
+  xscenario 'user is able to view the search engine bar' do
+
+  end
+
+  xscenario 'user is able to type a search query, submit the search and view results' do
+
+  end
+
+  xscenario 'user cannot search a blank search query' do
+
+  end
+end
